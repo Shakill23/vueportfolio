a4a27bc6b04093495327ac93c6310480b9247fce
Shakeel <shakeeldvd23@gmail.com>
2024-07-02 09:10:19 +0200

    Store and App Updated

--- src/App.vue
+++ src/App.vue
@@ -14,7 +14,39 @@ export default {
   components: {
     NavBar,
     Footer
-  }
+  },
+computed:{
+    getHome(){
+      return this.$store.dispatch("getHome")
+    },
+    getAbout(){
+      return this.$store.dispatch("getAbout")
+    },
+    getResume(){
+      return this.$store.dispatch("getResume")
+    },
+    getSkills(){
+      return this.$store.dispatch("getSkills")
+    },
+    getProjects(){
+      return this.$store.dispatch("getProjects")
+    },
+    getTestimonials(){
+      return this.$store.dispatch("getTestimonials")
+    },
+    getContact(){
+      return this.$store.dispatch("getContact")
+    }
+  },
+mounted(){
+  this.getHome
+  this.getAbout
+  this.getResume
+  this.getSkills
+  this.getProjects
+  this.getTestimonials
+  this.getContact
 }
+} 
 </script>
 <style src="@/assets/css/style.css"></style>
